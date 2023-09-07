d5f45b2feb99f4fd294136f08903cdf19867fd95
Matthew Warman <warmanmr@gmail.com>
2023-09-07 08:07:22 -0400

    inital storybook

--- src/components/Text/Text.types.ts
+++ src/components/Text/Text.types.ts
@@ -1,10 +1,10 @@
 import { PropsWithChildren } from 'react';
 
 export enum TextVariant {
-  Heading1,
-  Heading2,
-  Heading3,
-  BodyCopy,
+  Heading1='Heading1',
+  Heading2='Heading2',
+  Heading3='Heading3',
+  BodyCopy='BodyCopy',
 }
 
 export interface TextProps extends PropsWithChildren {
