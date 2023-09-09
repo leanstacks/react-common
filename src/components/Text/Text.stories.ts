6ad39352cd8f6ab8e6bc4805e74e896a6f986357
Matthew Warman <warmanmr@gmail.com>
2023-09-09 06:44:26 -0400

    integrate storybook with tailwind

--- src/components/Text/Text.stories.ts
+++ src/components/Text/Text.stories.ts
@@ -1,29 +1,59 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 
-import Text from "./Text";
-import { TextVariant } from "./Text.types";
+import Text from './Text';
+import { TextVariant } from './Text.types';
 
 const meta = {
-  title:  'Components/Text',
+  title: 'Components/Text',
   component: Text,
   parameters: {
-    layout: 'centered'
+    layout: 'centered',
   },
   tags: ['autodocs'],
   argTypes: {
-    children: {description: 'The text to display.'},
-    className: {description: 'Additional CSS classes.'},
-    variant: {control: 'select', options: [TextVariant.Heading1, TextVariant.Heading2, TextVariant.Heading3, TextVariant.BodyCopy], description: 'The variant style.'}
-  }
+    children: { description: 'The text to display.' },
+    className: { description: 'Additional CSS classes.' },
+    variant: {
+      control: 'select',
+      options: [
+        TextVariant.Heading1,
+        TextVariant.Heading2,
+        TextVariant.Heading3,
+        TextVariant.BodyCopy,
+      ],
+      description: 'The variant style.',
+    },
+  },
 } satisfies Meta<typeof Text>;
 
 export default meta;
 
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
-export const Heading1:Story = {
+export const Heading1: Story = {
   args: {
     children: 'The Grey Fox Jumps Over The Stream',
-    variant: TextVariant.Heading1
-  }
-}
+    variant: TextVariant.Heading1,
+  },
+};
+
+export const Heading2: Story = {
+  args: {
+    children: 'The Grey Fox Jumps Over The Stream',
+    variant: TextVariant.Heading2,
+  },
+};
+
+export const Heading3: Story = {
+  args: {
+    children: 'The Grey Fox Jumps Over The Stream',
+    variant: TextVariant.Heading3,
+  },
+};
+
+export const BodyCopy: Story = {
+  args: {
+    children: 'The Grey Fox Jumps Over The Stream',
+    variant: TextVariant.BodyCopy,
+  },
+};
