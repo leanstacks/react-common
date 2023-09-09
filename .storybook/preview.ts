16aa4a6752691e9e2ad67001d379fb63ac97eca8
Matthew Warman <warmanmr@gmail.com>
2023-09-09 07:19:24 -0400

    tailwind themes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,18 @@
 import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-styling';
 
 import './tailwind.css';
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-mode',
+  }),
+];
 
 const preview: Preview = {
   parameters: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   important: true,
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     extend: {},
   },
