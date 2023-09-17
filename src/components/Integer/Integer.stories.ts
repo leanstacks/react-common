97ce599f04d7a157fe3736a5feb965ecc496c1d2
Matthew Warman <warmanmr@gmail.com>
2023-09-17 15:47:29 -0400

    integer storybook stories

---
+++ src/components/Integer/Integer.stories.ts
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Integer from './Integer';
+import { Unit, UnitDisplay } from '../../utils';
+
+const meta = {
+  title: 'Components/Integer',
+  component: Integer,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    className: { description: 'Additional CSS classes.' },
+    testId: { description: 'Unit test identifier.' },
+    unit: {
+      control: 'select',
+      options: [
+        Unit.Celsius,
+        Unit.Centimeter,
+        Unit.Fahrenheit,
+        Unit.Inch,
+        Unit.Kilometer,
+        Unit.KilometerPerHour,
+        Unit.Mile,
+        Unit.MilePerHour,
+        Unit.Millimeter,
+        Unit.Percent,
+      ],
+      description: 'The unit of measurement.',
+    },
+    unitDisplay: {
+      control: 'select',
+      options: [UnitDisplay.Narrow, UnitDisplay.Short, UnitDisplay.Long],
+      description: 'The unit formatting style.',
+    },
+    value: { description: 'The number to be formatted and displayed.' },
+  },
+} satisfies Meta<typeof Integer>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Plain: Story = {
+  args: {
+    value: 23000,
+  },
+};
+
+export const Formatted: Story = {
+  args: {
+    value: 23000,
+    unit: Unit.MilePerHour,
+    unitDisplay: UnitDisplay.Narrow,
+  },
+};
