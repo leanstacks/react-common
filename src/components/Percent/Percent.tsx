80c18881a137c57730df6a68ea2e992e33401103
Matthew Warman <mwarman@users.noreply.github.com>
2023-09-29 16:16:28 -0400

    LSUITE-62 Add `Percent` component (#15)
    * LSUITE-62 initial Percent component
    
    * LSUITE-62 Percent props
    
    * LSUITE-62 docs
    
    * LSUITE-62 Percent tests
    
    * LSUITE-62 jsdoc

---
+++ src/components/Percent/Percent.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo } from 'react';
+
+import { PercentProps } from './Percent.types';
+import { formatNumber } from '../../utils/numbers';
+
+/**
+ * The `Percent` React component formats and renders a percent value.
+ * @param {PercentProps} props = Component properties.
+ * @returns {JSX.Element} JSX
+ */
+const Percent: React.FC<PercentProps> = ({
+  className,
+  maximumFractionDigits,
+  minimumFractionDigits,
+  signDisplay,
+  value,
+  testId = 'percent',
+}: PercentProps): JSX.Element => {
+  const val = useMemo(() => {
+    const formatOptions: Intl.NumberFormatOptions = {
+      style: 'percent',
+      maximumFractionDigits,
+      minimumFractionDigits,
+      signDisplay,
+    };
+    return formatNumber(value, formatOptions);
+  }, [value, maximumFractionDigits, minimumFractionDigits, signDisplay]);
+
+  return (
+    <span className={className} data-testid={testId}>
+      {val}
+    </span>
+  );
+};
+
+export default Percent;
