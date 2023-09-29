b036b20f4034a2505539667a67be2fc94dfb1d5b
Matthew Warman <warmanmr@gmail.com>
2023-09-29 15:03:36 -0400

    LSUITE-62 jsdoc

--- src/components/Percent/Percent.types.ts
+++ src/components/Percent/Percent.types.ts
@@ -1,18 +1,12 @@
+import { PropsWithClassName, PropsWithTestId } from '../../utils/types';
 import { SignDisplay } from '../../utils';
-
-export interface PropsWithTestId {
-  testId?: string;
-}
-
-export interface PropsWithClassName {
-  className?: string;
-}
 
 /**
  * Properties for the `Percent` component.
  * @param {number} [minimumFractionDigits] - Optional. The minimum number of fraction digits to use. Default: `0`
  * @param {number} [maximumFractionDigits] - Optional. The maximum number of fraction digits to use. Default: the larger of `minimumFractionDigits` and `0`
  * @param {SignDisplay} [signDisplay] - Optional. When to display the sign for the number. Default: `auto`
+ * @param {number} value - The percent as a decimal value, e.g. `0.34` renders 34%.
  */
 export interface PercentProps extends PropsWithClassName, PropsWithTestId {
   maximumFractionDigits?: number;
---
+++ src/utils/types.ts
@@ -0,0 +1,15 @@
+/**
+ * Component properties with test identifier.
+ * @param {string} [testId] - Optional. A testing library identifier.
+ */
+export interface PropsWithTestId {
+  testId?: string;
+}
+
+/**
+ * Component properties with CSS class name(s).
+ * @param {string} [className] - Optional. CSS class names.
+ */
+export interface PropsWithClassName {
+  className?: string;
+}
