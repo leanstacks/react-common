02167adb4c44d410485ad894c6682001f6cb2856
Matthew Warman <warmanmr@gmail.com>
2024-10-24 15:37:27 -0400

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,35 +5,22 @@ import { dts } from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-const packageJson = require('./package.json');
-
-export default [
+const config = [
   {
     input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      terser(),
-    ],
+    output: {
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), terser()],
     external: ['react'],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    input: 'dist/index.d.ts',
+    output: [{ file: 'dist/types/index.d.ts', format: 'es' }],
     plugins: [dts()],
   },
 ];
+
+export default config;
